54b6f03367d37a5dec5850adb795e2748bfc9cdf
David Timber <dxdt@dev.snart.me>
2024-01-12 19:46:07 +0900

    Modify KTCloud original rc.d scripts ...
    - Add src/common for nmcli parsing facilities
    - Remove all code dependent on dhclient files
    - Use nmcli to determine dhcp options
    - Remove use of logger as the scripts will be run under Systemd
    - Remove use of wget. Use curl instead

--- src/sethostname2.sh
+++ src/sethostname2.sh
@@ -1,14 +1,9 @@
-#!/bin/sh
-# dhclient change hostname script for Ubuntu
-#oldhostname=$(hostname -s)
-#if [ $oldhostname = 'localhost' ]
-#then
-#sleep 10 # Wait for configuration to be written to disk
-hostname=$(cat /var/lib/dhclient/dhclient--eth0.lease  |  awk ' /host-name/ { host = $3 }  END { printf host } ' | sed     's/[";]//g')
-# Update /etc/hosts
-# Rename Host
-echo $hostname > /etc/hostname
-hostname -b -F /etc/hostname
-echo $hostname > /proc/sys/kernel/hostname
-#fi
-#rm /etc/init.d/sethostname2.sh
+#!/bin/bash
+
+# Set the hostname to that of DHCP lease.
+
+set -e
+. "$(dirname "${BASH_SOURCE[0]}")/common"
+
+DHCP_HOST="$(get_dhcpopt host-name)"
+hostnamectl set-hostname "$DHCP_HOST"
